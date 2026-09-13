78c65fee2caca5f751e5373de5ed075763d1e1c9
Kaztaztrophe <kaztaztrophe@gmail.com>
2026-09-13 17:58:40 -0400

    Update wolvhelpermini_encounters.user.js

--- wolvhelpermini_encounters.user.js
+++ wolvhelpermini_encounters.user.js
@@ -826,12 +826,19 @@
 			return;
 		}
 
+		const oldHTML = output.innerHTML;
+		let changed = false;
+
 		const observer = new MutationObserver(() => {
+			if (changed || output.innerHTML === oldHTML) {
+				return;
+			}
+
+			changed = true;
 			observer.disconnect();
 
 			requestAnimationFrame(() => {
 				updateExploreOutput();
-				waitForExploreChange();
 			});
 		});
 
